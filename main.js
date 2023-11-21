08d1714758174fef6000d4c87e1500877ed89155
Jhojan Stiwer <stiwercc@gmail.com>
2023-11-21 16:50:41 -0500

    Plantilla 2 completada

--- main.js
+++ main.js
@@ -14,3 +14,12 @@ document.addEventListener("DOMContentLoaded", function () {
         contactoPopup.style.display = "none";
     });
 });
+
+function closePopup(popupId) {
+    const popup = document.getElementById(popupId);
+    popup.style.animation = 'fadeOutUp 0.5s ease-in-out'; // Aplica la animación de salida
+    setTimeout(() => {
+        popup.style.display = 'none';
+        popup.style.animation = ''; // Restaura la animación
+    }, 500); // Tiempo de espera igual al tiempo de la animación
+}
